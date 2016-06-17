1898f3594c5844f555b77169eeb4575b32a5e69d
drewbroc <andrewbrockerhoff@me.com>
2016-06-17 11:11:20 -0400

    na.gif test debug

---
+++ README.md
@@ -0,0 +1,7 @@
+cucumberjvm-selenium-pageObject
+=====================
+
+Page Object Framework using cucumber jvm (java) and Selenium java.
+
+Item_Setup - Generates information from website brand and item pages and extracts to xlsx
+Other tests - Uses xlsx to drive tests
---
+++ src/test/java/com/famous_smoke/automation/step_definitions/validation/ThenSteps.java
@@ -0,0 +1,32 @@
+package com.famous_smoke.automation.step_definitions.validation;
+
+
+import com.famous_smoke.automation.Hooks;
+import com.famous_smoke.automation.pageobjects.BrandItemPage;
+
+import cucumber.api.java.en.Then;
+import static com.famous_smoke.automation.assertions.FamousSmokeAssertions.assertThat;
+
+
+/**
+ * Created by drew on 11-01-2016.
+ */
+public class ThenSteps {
+
+    @Then("^the brand identification should be visible$")
+    public void the_brand_identification_should_be_visisble() throws Throwable {
+        assertThat(Hooks.testBrandPageData).isIdentified();
+    }
+
+    @Then("^the item identification should be visible$")
+    public void the_item_identification_should_be_visible() throws Throwable {
+        assertThat(Hooks.testBrandItemPageData).isIdentified();
+    }
+
+    //Added by drew.com as na.gif validator
+    @Then("^the page should not have any na.gif image$")
+    public void the_page_does_not_have_any_na_gif_image() throws Throwable {
+        assertThat(BrandItemPage.hasNaImage()).isFalse();
+    }
+
+}
