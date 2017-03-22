037bb80b0679caac5d8bc301212b8ae89c94f7bf
drewbroc <abrocker@gmail.com>
2017-03-23 00:24:51 +0530

    Spreadsheet column added.

--- src/test/java/com/famous_smoke/automation/data/DataWorkbook.java
+++ src/test/java/com/famous_smoke/automation/data/DataWorkbook.java
@@ -63,6 +63,7 @@ public final class DataWorkbook {
     private static final String BRAND_IDENTIFIED_HEADER        = BrandPageData.IDENTIFIED_FIELD_NAME;
     private static final String BRAND_INDEX_HEADER             = BrandPageData.INDEX_FIELD_NAME;
     private static final String BRAND_NAGIF_HEADER             = BrandPageData.NAGIF_FIELD_NAME;
+    private static final String BRAND_ALT_TAG_HEADER           = BrandPageData.ALT_TAG_FIELD_NAME;
 
     /**
      * The header row information for the BrandItemPage
@@ -82,6 +83,7 @@ public final class DataWorkbook {
     private static final String ITEM_IDENTIFIED_HEADER        = BrandItemPageData.IDENTIFIED_FIELD_NAME;
     private static final String ITEM_INDEX_HEADER             = BrandItemPageData.INDEX_FIELD_NAME;
     private static final String ITEM_NAGIF_HEADER             = BrandItemPageData.NAGIF_FIELD_NAME;
+    private static final String ITEM_ALT_TAG_HEADER           = BrandItemPageData.ALT_TAG_FIELD_NAME;
     
     /**
      * The column placements of the BrandPageData elements
@@ -432,7 +434,8 @@ public final class DataWorkbook {
                 {BRAND_BREADCRUMBS_TEXT_COLUMN, BRAND_BREADCRUMBS_TEXT_HEADER},
                 {BRAND_BREADCRUMBS_LINKS_COLUMN, BRAND_BREADCRUMBS_LINKS_HEADER},
                 {BRAND_IDENTIFIED_COLUMN, BRAND_IDENTIFIED_HEADER},
-                {BRAND_NAGIF_COLUMN, BRAND_NAGIF_HEADER}
+                {BRAND_NAGIF_COLUMN, BRAND_NAGIF_HEADER},
+                {BRAND_ALT_TAG_COLUMN, BRAND_ALT_TAG_HEADER}
         }).collect(Collectors.toMap(kv -> (Integer) kv[0], kv -> (String) kv[1]));
     }
 
@@ -451,7 +454,8 @@ public final class DataWorkbook {
                 {ITEM_BREADCRUMBS_TEXT_COLUMN, ITEM_BREADCRUMBS_TEXT_HEADER},
                 {ITEM_BREADCRUMBS_LINKS_COLUMN, ITEM_BREADCRUMBS_LINKS_HEADER},
                 {ITEM_IDENTIFIED_COLUMN, ITEM_IDENTIFIED_HEADER},
-                {ITEM_NAGIF_COLUMN, ITEM_NAGIF_HEADER}
+                {ITEM_NAGIF_COLUMN, ITEM_NAGIF_HEADER},
+                {ITEM_ALT_TAG_COLUMN, ITEM_ALT_TAG_HEADER}
         }).collect(Collectors.toMap(kv -> (Integer) kv[0], kv -> (String) kv[1]));
     }
 
@@ -475,7 +479,8 @@ public final class DataWorkbook {
                 {BRAND_BREADCRUMBS_TEXT_COLUMN, data.getBreadcrumbsText()},
                 {BRAND_BREADCRUMBS_LINKS_COLUMN, reduceCollectionToString(data.getBreadcrumbsLinks())},
                 {BRAND_IDENTIFIED_COLUMN, data.getIdentified().toString()},
-                {BRAND_NAGIF_COLUMN, data.getNaGif().toString()}
+                {BRAND_NAGIF_COLUMN, data.getNaGif().toString()},
+                {BRAND_ALT_TAG_COLUMN, data.getAltTag()}
         }).collect(Collectors.toMap(kv -> (Integer) kv[0], kv -> (String) kv[1]));
     }
 
@@ -502,7 +507,8 @@ public final class DataWorkbook {
                 {ITEM_BREADCRUMBS_TEXT_COLUMN, data.getBreadcrumbsText()},
                 {ITEM_BREADCRUMBS_LINKS_COLUMN, reduceCollectionToString(data.getBreadcrumbsLinks())},
                 {ITEM_IDENTIFIED_COLUMN, data.getIdentified().toString()},
-                {ITEM_NAGIF_COLUMN, data.getNaGif().toString()}
+                {ITEM_NAGIF_COLUMN, data.getNaGif().toString()},
+                {ITEM_ALT_TAG_COLUMN, data.getAltTag()}
         }).collect(Collectors.toMap(kv -> (Integer) kv[0], kv -> (String) kv[1]));
     }
 
